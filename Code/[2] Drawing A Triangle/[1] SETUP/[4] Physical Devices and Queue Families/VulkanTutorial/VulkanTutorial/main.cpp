7160467796c519d6a6f55d9c759f019412147d67
Sonar Systems <SonarSystems@users.noreply.github.com>
2021-08-02 11:41:12 +0100

    Physical Device and Logical Device Code

---
+++ Code/[2] Drawing A Triangle/[1] SETUP/[4] Physical Devices and Queue Families/VulkanTutorial/VulkanTutorial/main.cpp
@@ -0,0 +1,366 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <vector>
+/**
+* \brief START OF NEW CODE ADDED
+*
+* New headers
+*/
+#include <optional>
+/**
+* \brief END OF NEW CODE ADDED
+*/
+
+const unsigned int WIDTH = 800;
+const unsigned int HEIGHT = 600;
+
+const std::vector<const char *> validationLayers =
+{
+	"VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
+
+VkResult CreateDebugUtilsMessengerEXT( VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT *pCreateInfo, const VkAllocationCallbacks *pAllocator, VkDebugUtilsMessengerEXT *pDebugMessenger )
+{
+	auto func = ( PFN_vkCreateDebugUtilsMessengerEXT )vkGetInstanceProcAddr( instance, "vkCreateDebugUtilsMessengerEXT" );
+
+	if ( func != nullptr )
+	{
+		return func( instance, pCreateInfo, pAllocator, pDebugMessenger );
+	}
+	else
+	{
+		return VK_ERROR_EXTENSION_NOT_PRESENT;
+	}
+}
+
+void DestroyDebugUtilsMessengerEXT( VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks *pAllocator )
+{
+	auto func = ( PFN_vkDestroyDebugUtilsMessengerEXT )vkGetInstanceProcAddr( instance, "vkDestroyUtilsMessengerEXT" );
+
+	if ( func != nullptr )
+	{
+		func( instance, debugMessenger, pAllocator );
+	}
+}
+
+/**
+* \brief START OF NEW CODE ADDED
+*
+* Graphics families
+*/
+struct QueueFamilyIndices
+{
+	std::optional<uint32_t> graphicsFamily;
+
+	bool IsComplete( )
+	{
+		return graphicsFamily.has_value( );
+	}
+};
+/**
+* \brief END OF NEW CODE ADDED
+*/
+
+class HelloTriangleApplication
+{
+public:
+	void Run( )
+	{
+		InitWindow( );
+		InitVulkan( );
+		MainLoop( );
+		Cleanup( );
+	}
+
+private:
+	GLFWwindow *_window;
+	VkInstance _instance;
+	VkDebugUtilsMessengerEXT _debugMessenger;
+
+	/**
+	* \brief START OF NEW CODE ADDED
+	*
+	* Physical graphics device to use
+	*/
+	VkPhysicalDevice _physicalDevice = VK_NULL_HANDLE;
+	/**
+	* \brief END OF NEW CODE ADDED
+	*/
+
+	void InitWindow( )
+	{
+		glfwInit( );
+
+		glfwWindowHint( GLFW_CLIENT_API, GLFW_NO_API );
+		glfwWindowHint( GLFW_RESIZABLE, GLFW_FALSE );
+
+		_window = glfwCreateWindow( WIDTH, HEIGHT, "Vulkan", nullptr, nullptr );
+	}
+
+	void InitVulkan( )
+	{
+		CreateInstance( );
+		SetupDebugMessenger( );
+
+		/**
+		* \brief START OF NEW CODE ADDED
+		*
+		* Call the pick physical device method
+		*/
+		PickPhysicalDevice( );
+		/**
+		* \brief END OF NEW CODE ADDED
+		*/
+	}
+
+	void MainLoop( )
+	{
+		while ( !glfwWindowShouldClose( _window ) )
+		{
+			glfwPollEvents( );
+		}
+	}
+
+	void Cleanup( )
+	{
+		if ( enableValidationLayers )
+		{
+			DestroyDebugUtilsMessengerEXT( _instance, _debugMessenger, nullptr );
+		}
+
+		vkDestroyInstance( _instance, nullptr );
+		glfwDestroyWindow( _window );
+		glfwTerminate( );
+	}
+
+	void CreateInstance( )
+	{
+		if ( enableValidationLayers && !CheckValidationLayerSupport( ) )
+		{
+			throw std::runtime_error( "Validation layers requested, but not available" );
+		}
+
+		VkApplicationInfo appInfo{ };
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION( 1, 0, 0 );
+		appInfo.pEngineName = "SoSy Game Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION( 1, 0, 0 );
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+		VkInstanceCreateInfo createInfo{ };
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+
+		auto extensions = GetRequiredExtensions( );
+		createInfo.enabledExtensionCount = static_cast<unsigned int>( extensions.size( ) );
+		createInfo.ppEnabledExtensionNames = extensions.data( );
+
+		VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{ };
+
+		if ( enableValidationLayers )
+		{
+			createInfo.enabledLayerCount = static_cast<unsigned int>( validationLayers.size( ) );
+			createInfo.ppEnabledLayerNames = validationLayers.data( );
+
+			PopulateDebugMessengerCreateInfo( debugCreateInfo );
+			createInfo.pNext = ( VkDebugUtilsMessengerCreateInfoEXT * )&debugCreateInfo;
+		}
+		else
+		{
+			createInfo.enabledLayerCount = 0;
+			createInfo.pNext = nullptr;
+		}
+
+		if ( vkCreateInstance( &createInfo, nullptr, &_instance ) != VK_SUCCESS )
+		{
+			throw std::runtime_error( "Failed to create Vulkan Instance!" );
+		}
+		/*
+		std::vector<VkExtensionProperties> extensions( glfwExtentionCount );
+
+		vkEnumerateInstanceExtensionProperties( nullptr, &glfwExtentionCount, extensions.data( ) );
+
+		std::cout << "Available Extensions: " << std::endl;
+
+		for ( const auto &extension : extensions )
+		{
+			std::cout << '\t' << extension.extensionName << std::endl;
+		}*/
+	}
+
+	void PopulateDebugMessengerCreateInfo( VkDebugUtilsMessengerCreateInfoEXT &createInfo )
+	{
+		createInfo = {};
+		createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+		createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+		createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+		createInfo.pfnUserCallback = DebugCallback;
+	}
+
+	void SetupDebugMessenger( )
+	{
+		if ( !enableValidationLayers )
+		{ return; }
+
+		VkDebugUtilsMessengerCreateInfoEXT createInfo;
+		PopulateDebugMessengerCreateInfo( createInfo );
+
+		if ( CreateDebugUtilsMessengerEXT( _instance, &createInfo, nullptr, &_debugMessenger ) != VK_SUCCESS )
+		{
+			throw std::runtime_error( "Failed to setup up debug messenger" );
+		}
+	}
+
+	/**
+	* \brief START OF NEW CODE ADDED
+	*
+	* Choosing the right device for Vulkan
+	*/
+	void PickPhysicalDevice( )
+	{
+		uint32_t deviceCount = 0;
+		vkEnumeratePhysicalDevices( _instance, &deviceCount, nullptr );
+
+		if ( deviceCount == 0 )
+		{
+			throw std::runtime_error( "Failed to find any GPUs with Vulkan support!" );
+		}
+
+		std::vector<VkPhysicalDevice> devices( deviceCount );
+		vkEnumeratePhysicalDevices( _instance, &deviceCount, devices.data( ) );
+
+		for ( const auto &device : devices )
+		{
+			if ( IsDeviceSuitable( device ) )
+			{
+				_physicalDevice = device;
+
+				break;
+			}
+		}
+
+		if ( _physicalDevice == VK_NULL_HANDLE )
+		{
+			throw std::runtime_error( "Failed to find a suitable GPU!" );
+		}
+	}
+
+	bool IsDeviceSuitable( VkPhysicalDevice device )
+	{
+		QueueFamilyIndices indices = FindQueueFamilies( device );
+
+		return indices.IsComplete( );
+	}
+
+	QueueFamilyIndices FindQueueFamilies( VkPhysicalDevice device )
+	{
+		QueueFamilyIndices indices;
+
+		uint32_t queueFamilyCount = 0;
+		vkGetPhysicalDeviceQueueFamilyProperties( device, &queueFamilyCount, nullptr );
+
+		std::vector<VkQueueFamilyProperties> queueFamilies( queueFamilyCount );
+		vkGetPhysicalDeviceQueueFamilyProperties( device, &queueFamilyCount, queueFamilies.data( ) );
+
+		int i = 0;
+		for ( const auto &queueFamily : queueFamilies )
+		{
+			if ( queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT )
+			{
+				indices.graphicsFamily = i;
+			}
+
+			if ( indices.IsComplete( ) )
+			{
+				break;
+			}
+
+			i++;
+		}
+
+		return indices;
+	}
+	/**
+	* \brief END OF NEW CODE ADDED
+	*/
+
+	std::vector<const char *>GetRequiredExtensions( )
+	{
+		unsigned int glfwExtensionCount = 0;
+		const char **glfwExtensions;
+		glfwExtensions = glfwGetRequiredInstanceExtensions( &glfwExtensionCount );
+
+		std::vector<const char*> extensions( glfwExtensions, glfwExtensions + glfwExtensionCount );
+
+		if ( enableValidationLayers )
+		{
+			extensions.push_back( VK_EXT_DEBUG_UTILS_EXTENSION_NAME );
+		}
+
+		return extensions;
+	}
+
+	bool CheckValidationLayerSupport( )
+	{
+		unsigned int layerCount;
+		vkEnumerateInstanceLayerProperties( &layerCount, nullptr );
+
+		std::vector<VkLayerProperties> availableLayers( layerCount );
+		vkEnumerateInstanceLayerProperties( &layerCount, availableLayers.data( ) );
+
+		for ( const char *layerName : validationLayers )
+		{
+			bool layerFound = false;
+
+			for ( const auto &layerProperties : availableLayers )
+			{
+				if ( strcmp( layerName, layerProperties.layerName ) == 0 )
+				{
+					layerFound = true;
+					break;
+				}
+			}
+
+			if ( !layerFound )
+			{ return false; }
+		}
+
+		return true;
+	}
+
+	static VKAPI_ATTR VkBool32 VKAPI_CALL DebugCallback( VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData, void *pUserData )
+	{
+		std::cerr << "Validation layer: " << pCallbackData->pMessage << std::endl;
+
+		return VK_FALSE;
+	}
+};
+
+int main( )
+{
+	HelloTriangleApplication app;
+
+	try
+	{
+		app.Run( );
+	}
+	catch ( const std::exception &e )
+	{
+		std::cerr << e.what( ) << std::endl;
+
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Vulkan Tutorials
+version https://git-lfs.github.com/spec/v1
+oid sha256:faaa21710f03607546f639101d679d808386c4dcd5f48859cd311786c9a89b22
+size 19
